c8950efe601001f992259d44d6bc3249637f6164
Michael Herrera <Michael Herrera>
2022-11-05 22:16:06 -0500

    formulario-en-validacion

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-persona',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent implements OnInit {
-
-  constructor() { }
+    items: any;
+    checkoutForm: any;
+  constructor(private formBuilder: FormBuilder,) { 
+    this.checkoutForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      nit:['', Validators.required],
+      pais:['', Validators.required],
+      departamento:['', Validators.required],
+      ciudad:['', Validators.required],
+    });
+    
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(customerData) {
+
+    console.warn('Your order has been submitted', customerData);
   }
 
 }
